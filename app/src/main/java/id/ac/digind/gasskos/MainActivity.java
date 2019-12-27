04208f6479105a76c66a5b442b191f167bf3b671
Labba Awwabi <labbaawwabi@gmail.com>
2019-12-27 22:03:58 +0700

    update Rinda

--- app/src/main/java/id/ac/digind/gasskos/MainActivity.java
+++ app/src/main/java/id/ac/digind/gasskos/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, RiwayatActivity.class));
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 finish();
             }
         },1000);
