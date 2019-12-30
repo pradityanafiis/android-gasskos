ca1b980eafd94514e139dfd97e850cc7c3e28db3
Labba Awwabi <labbaawwabi@gmail.com>
2019-12-30 19:27:41 +0700

    upadate baruu

--- app/src/main/java/id/ac/digind/gasskos/DetailKostActivity.java
+++ app/src/main/java/id/ac/digind/gasskos/DetailKostActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.smarteist.autoimageslider.IndicatorAnimations;
 import com.smarteist.autoimageslider.SliderAnimations;
@@ -14,7 +17,10 @@ import com.smarteist.autoimageslider.SliderView;
 
 import id.ac.digind.gasskos.adapters.SliderAdapter;
 
-public class DetailKostActivity extends AppCompatActivity {
+public class DetailKostActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button bDetail, bFoto;
+    private TextView tvDetailKos;
+    private RecyclerView rvDetailFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,14 @@ public class DetailKostActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        bDetail = findViewById(R.id.b_detail);
+        bFoto = findViewById(R.id.b_foto);
+        tvDetailKos = findViewById(R.id.tv_detailkos);
+        rvDetailFoto = findViewById(R.id.rv_detailfoto);
+
+        bDetail.setOnClickListener(this);
+        bFoto.setOnClickListener(this);
 
         SliderView sliderView = findViewById(R.id.imageSlider);
 
@@ -39,4 +53,18 @@ public class DetailKostActivity extends AppCompatActivity {
         sliderView.startAutoCycle();
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.b_detail) {
+            bDetail.setBackgroundResource(R.drawable.tepi5);
+            bFoto.setBackgroundResource(R.drawable.tepi6);
+            tvDetailKos.setVisibility(View.VISIBLE);
+            rvDetailFoto.setVisibility(View.GONE);
+        }else if (v.getId() == R.id.b_foto) {
+            bDetail.setBackgroundResource(R.drawable.tepi6);
+            bFoto.setBackgroundResource(R.drawable.tepi5);
+            tvDetailKos.setVisibility(View.GONE);
+            rvDetailFoto.setVisibility(View.VISIBLE);
+        }
+    }
 }
