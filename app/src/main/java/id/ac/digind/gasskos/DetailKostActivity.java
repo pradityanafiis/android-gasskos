6c96f9a792ef6100363239b8377432332a4755f8
Labba Awwabi <labbaawwabi@gmail.com>
2019-12-30 19:00:51 +0700

    add slider

--- app/src/main/java/id/ac/digind/gasskos/DetailKostActivity.java
+++ app/src/main/java/id/ac/digind/gasskos/DetailKostActivity.java
@@ -1,11 +1,18 @@
 package id.ac.digind.gasskos;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import id.ac.digind.gasskos.adapters.SliderAdapter;
 
 public class DetailKostActivity extends AppCompatActivity {
 
@@ -16,6 +23,20 @@ public class DetailKostActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        SliderView sliderView = findViewById(R.id.imageSlider);
+
+        SliderAdapter adapter = new SliderAdapter(this);
+
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
+        sliderView.startAutoCycle();
     }
 
 }
