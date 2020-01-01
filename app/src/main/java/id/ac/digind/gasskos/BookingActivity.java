c90bc6e4215419e4e04a3636b2d614acd270f320
Labba Awwabi <labbaawwabi@gmail.com>
2020-01-01 13:57:10 +0700

    update back booking

--- app/src/main/java/id/ac/digind/gasskos/BookingActivity.java
+++ app/src/main/java/id/ac/digind/gasskos/BookingActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,19 +21,18 @@ public class BookingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_booking);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Kamar kamar = getIntent().getParcelableExtra(extra_kamar);
-        Toast.makeText(this, kamar.getTipe(), Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
